4cc408d330435d9d4aab7fffc024e7e01363d9f3
DonnaMayAlcos <donnamay.alcos21@gmail.com>
2025-04-21 10:53:54 +0800

    feat(auth): Forgot Password page fully implemented with email input, reset button, and social login options

--- frontend/src/app/(auth)/forgotpassword/page.tsx
+++ frontend/src/app/(auth)/forgotpassword/page.tsx
@@ -1,10 +1,10 @@
-import { LoginForm } from "@/components/ui/login-form"
+import { ForgotPasswordForm } from "@/components/ui/forgotpassword-form"
 
-export default function LoginPage() {
+export default function ForgotPasswordPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-lg flex-col gap-6">
-        <LoginForm />
+        <ForgotPasswordForm />
       </div>
     </div>
   )
