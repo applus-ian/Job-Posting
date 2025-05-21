e98aab969f6de8c9605884b5b3c45036614641ea
saydeee <insik.smarie@gmail.com>
2025-05-21 12:25:53 +0800

    fix(ui): edited the calendar view and list view design

--- frontend/src/components/badges/CustomBadge.tsx
+++ frontend/src/components/badges/CustomBadge.tsx
@@ -39,6 +39,22 @@ const statusStyles = {
     text: "text-[#00730D]",
     bg: "bg-[#B6FFB2]",
   },
+  pending: {
+  text: "text-dark", 
+  bg: "bg-yellow-200",     
+  },
+  upcoming: {
+    text: "text-[#005173]",
+    bg: "bg-[#C8F5FF]",  
+  },
+  completed: {
+    text: "text-[#00730D]",
+    bg: "bg-[#B6FFB2]",
+  },
+  cancelled: {
+    text: "text-[#FF0000]",
+    bg: "bg-[#FFE6E6]",
+  },
 };
 
 export default function CustomBadge({
